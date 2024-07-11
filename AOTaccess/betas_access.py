95364006a93cab14e4b7b2072ee4eec881b1935e
Shufan <shufan.z.t@gmail.com>
2024-07-12 01:16:04 +0200

    /

--- AOTaccess/betas_access.py
+++ AOTaccess/betas_access.py
@@ -6,3 +6,15 @@ import yaml
 
 basedir = Path(__file__).resolve().parent
 settings = yaml.safe_load(open(basedir / 'settings.yml'))
+glmsingle_dir = Path(settings['paths']['glmsingle'])
+
+def get_glmsingle_dir(sub,ses,stctype = "nordicstc"):
+    sub = str(sub).zfill(3)
+    ses = str(ses).zfill(2)
+    return glmsingle_dir / f'sub-{sub}_ses-{ses}_T1W_{stctype}_mainfull'
+
+def get_nii_path(sub,ses,stctype = "nordicstc",glmtype = "TYPED_FITHRF_GLMDENOISE_RR"):
+    glmsingle_dir = get_glmsingle_dir(sub,ses,stctype)
+    glm_type_dir = glmsingle_dir / glmtype
+    
+    
