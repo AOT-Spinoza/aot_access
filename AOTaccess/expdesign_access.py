5ed8765247847a153a209573b61735f86851ab45
Shufan <shufan.z.t@gmail.com>
2024-09-25 21:47:10 +0200

    class for all function

--- AOTaccess/expdesign_access.py
+++ AOTaccess/expdesign_access.py
@@ -4,5 +4,11 @@ import sys
 import os
 import yaml
 
-basedir = Path(__file__).resolve().parent
-settings = yaml.safe_load(open(basedir / "settings.yml"))
+
+class ExpDesignAccess:
+    def __init__(self, sub, ses):
+        basedir = Path(__file__).resolve().parent
+        settings = yaml.safe_load(open(basedir / "settings.yml"))
+
+        self.sub = sub
+        self.ses = ses
--- AOTaccess/stimuliinfo_access.py
+++ AOTaccess/stimuliinfo_access.py
@@ -4,18 +4,22 @@ import sys
 import os
 import yaml
 
-basedir = Path(__file__).resolve().parent
-settings = yaml.safe_load(open(basedir / "settings.yml"))
-video_dir = Path(settings["paths"]["videos"])
-video_annotation_dir = Path(settings["paths"]["video_annotations"])
-print(video_dir)
 
+class StimuliInfoAccess:
+    def __init__(self, sub, ses):
+        basedir = Path(__file__).resolve().parent
+        settings = yaml.safe_load(open(basedir / "settings.yml"))
+        self.video_dir = Path(settings["paths"]["videos"])
+        self.video_annotation_dir = Path(settings["paths"]["video_annotations"])
+        self.sub = sub
+        self.ses = ses
 
-def get_video_path(video_id, direction="fw"):  # fw and rv
-    return video_dir / f"{video_id}_{direction}.mp4"
+    def get_video_path(self, video_id, direction="fw"):
+        return self.video_dir / f"{video_id}_{direction}.mp4"
 
-
-def get_video_annotation_dir(
-    video_id, direction="fw", annotation_type="semantic_segmentation"
-):  # fw and rv
-    return video_annotation_dir / f"{video_id}_{direction}.mp4" / annotation_type
+    def get_video_annotation_dir(
+        self, video_id, direction="fw", annotation_type="semantic_segmentation"
+    ):
+        return (
+            self.video_annotation_dir / f"{video_id}_{direction}.mp4" / annotation_type
+        )
