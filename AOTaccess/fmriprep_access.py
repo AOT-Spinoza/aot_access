074fc7d57128383ffbb1d04a7341fd4b9bf7cb6c
Shufan <shufan.z.t@gmail.com>
2024-10-23 23:59:27 +0200

    fmriprep path

--- AOTaccess/fmriprep_access.py
+++ AOTaccess/fmriprep_access.py
@@ -16,7 +16,7 @@ class FmriprepAccess:
         example : sub-001_ses-01_task-AOT_rec-nordicstc_run-1_space-T1w_desc-preproc_part-mag_bold.nii.gz
         """
         bold_file = (
-            self.fmriprep_dir
+            self.root_fmriprep_dir
             / f"sub-{sub:03d}"
             / f"ses-{ses:02d}"
             / f"sub-{sub:03d}_ses-{ses:02d}_task-AOT_rec-nordicstc_run-{run}_space-T1w_desc-preproc_part-mag_bold.nii.gz"
@@ -32,7 +32,7 @@ class FmriprepAccess:
         example : sub-001_ses-01_task-AOT_rec-nordicstc_run-1_space-T1w_part-mag_boldref.nii.gz
         """
         boldref_file = (
-            self.fmriprep_dir
+            self.root_fmriprep_dir
             / f"sub-{sub:03d}"
             / f"ses-{ses:02d}"
             / f"sub-{sub:03d}_ses-{ses:02d}_task-AOT_rec-nordicstc_run-{run}_space-T1w_part-mag_boldref.nii.gz"
@@ -49,7 +49,7 @@ class FmriprepAccess:
         """
 
         brain_mask_file = (
-            self.fmriprep_dir
+            self.root_fmriprep_dir
             / f"sub-{sub:03d}"
             / f"ses-{ses:02d}"
             / f"sub-{sub:03d}_ses-{ses:02d}_task-AOT_run-1_space-T1w_desc-brain_mask.nii.gz"
