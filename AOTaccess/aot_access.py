3344e0a5aa3b4d2f1fbfc096c52d9e8919a1cee6
Shufan <shufan.z.t@gmail.com>
2025-02-10 20:12:14 +0000

    final interface

--- AOTaccess/aot_access.py
+++ AOTaccess/aot_access.py
@@ -17,3 +17,53 @@ class AOTAccess:
         self.glmsingle_access = GLMSingleAccess()
         self.expdesign_access = ExpDesignAccess()
         self.stimuli_info_access = StimuliInfoAccess()
+
+    def get_affine_header(sub: int):
+        pass
+
+    def get_meanvol_from_session(sub: int, ses: int, resolution: str = "1.7mm"):
+        pass
+
+    def get_betas_from_session(
+        sub: int,
+        ses: int,
+        glmtype: str = "TYPED_FITHRF_GLMDENOISE_RR",
+        resolution: str = "1.7mm",
+    ):
+        pass
+
+    def get_betas_from_video(
+        sub: int,
+        video: int,
+        glmtype: str = "TYPED_FITHRF_GLMDENOISE_RR",
+        resolution: str = "1.7mm",
+    ):
+        pass
+
+    def get_R2_from_session(
+        sub: int,
+        ses: int,
+        glmtype: str = "TYPED_FITHRF_GLMDENOISE_RR",
+        resolution: str = "1.7mm",
+    ):
+        pass
+
+    def get_R2_mask_from_session(
+        sub: int,
+        ses: int,
+        threshold: float = 0.2,
+        glmtype: str = "TYPED_FITHRF_GLMDENOISE_RR",
+        resolution: str = "1.7mm",
+    ):
+        pass
+
+    def get_video_list_from_session(sub: int, ses: int):
+        pass
+
+    def get_session_from_video(sub: int, video: int):
+        pass
+
+    def get_preproced_bold_from_session(
+        sub: int, ses: int, run: int, resolution: str = "1.7mm"
+    ):
+        pass
